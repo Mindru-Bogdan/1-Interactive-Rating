b009bd96276ddddf4ae3437e1dbbacd75d52e554
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-03-29 15:50:23 +0300

    Post initial files

---
+++ src/Components/Rating.js
@@ -0,0 +1,20 @@
+const Rating = () => {
+  return (
+    <div>
+      {/* <!-- Rating state start --> */}
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All
+        feedback is appreciated to help us improve our offering!
+      </p>
+
+      <p>1 2 3 4 5</p>
+
+      <p>Submit</p>
+
+      {/* <!-- Rating state end --> */}
+    </div>
+  );
+};
+
+export default Rating;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Rating from './Components/Rating';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Fragment>
+    <Rating />
+  </Fragment>
+);
