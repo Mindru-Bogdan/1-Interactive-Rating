b6ceff9a8ef6334ccca557fdb61c7f1f523c7890
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-04-18 18:09:06 +0300

    Rating almost done

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,20 +1,37 @@
+import { useState } from 'react';
+
 import classes from './Rating.module.css';
 import StarLogo from '../../src/icon-star.svg';
 
 const Rating = () => {
+  const [select, setSelect] = useState(false);
+
+  const selectHandler = () => {
+    setSelect(!select);
+  };
+
   return (
     <div className={classes.square}>
       {/* <!-- Rating state start --> */}
-      <img src={StarLogo}></img>
+      <img
+        src={StarLogo}
+        className={classes.star}
+      ></img>
       <h1>How did we do?</h1>
-      <p>
+      <p className={classes.paragraph}>
         Please let us know how we did with your support request. All
         feedback is appreciated to help us improve our offering!
       </p>
 
-      <p>1 2 3 4 5</p>
+      <ul className={select ? classes.selected : classes.select}>
+        <li onClick={selectHandler}>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
 
-      <button className={classes.submit}>Submit</button>
+      <button className={classes.submit}>SUBMIT</button>
 
       {/* <!-- Rating state end --> */}
     </div>
